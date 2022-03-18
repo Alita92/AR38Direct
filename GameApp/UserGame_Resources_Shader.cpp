de1da89d5cbbf891148e07e5197a5a69f1a7901c
Won Park <nerichagi21@gmail.com>
2022-03-18 15:41:23 +0900

    유저 리소스 쉐이더 일단 막기

--- GameApp/UserGame_Resources_Shader.cpp
+++ GameApp/UserGame_Resources_Shader.cpp
@@ -9,29 +9,29 @@ void AppShaderLoad()
 	GameEngineDirectory Dir;
 	Dir.MoveParent("AR38Direct");
 	Dir.MoveChild("Resources");
-	Dir.MoveChild("Shader");
-
-	std::vector<GameEngineFile> AllShader = Dir.GetAllFile("fx");
-
-	for (auto& ShaderFile : AllShader)
-	{
-		ShaderFile.Open("rt");
-		// 오픈모드
-		// rt : 텍스트 파일로 읽어들인다
-
-		std::string FileName = ShaderFile.GetFileNameWithOutExtension();
-		std::string AllCode = ShaderFile.GetString(); // 파일 내의 모든 텍스트를 가져온다.
-
-		if (std::string::npos != AllCode.find(FileName + "_VS")) // npos = -1 로 정의된 특수값, "find() 값을 못 찾을시 npos 리턴된다!"
-		{
-			GameEngineVertexShader* Ptr = GameEngineVertexShaderManager::GetInst().Load(FileName + "_VS", ShaderFile.GetFullPath(), FileName + "_VS");
-		}
-
-		if (std::string::npos != AllCode.find(FileName + "_PS"))
-		{
-			GameEnginePixelShader* Ptr = GameEnginePixelShaderManager::GetInst().Load(FileName + "_PS", ShaderFile.GetFullPath(), FileName + "_PS");
-		}
-	}
+	//Dir.MoveChild("Shader");
+	//
+	//std::vector<GameEngineFile> AllShader = Dir.GetAllFile("fx");
+	//
+	//for (auto& ShaderFile : AllShader)
+	//{
+	//	ShaderFile.Open("rt");
+	//	// 오픈모드
+	//	// rt : 텍스트 파일로 읽어들인다
+	//
+	//	std::string FileName = ShaderFile.GetFileNameWithOutExtension();
+	//	std::string AllCode = ShaderFile.GetString(); // 파일 내의 모든 텍스트를 가져온다.
+	//
+	//	if (std::string::npos != AllCode.find(FileName + "_VS")) // npos = -1 로 정의된 특수값, "find() 값을 못 찾을시 npos 리턴된다!"
+	//	{
+	//		GameEngineVertexShader* Ptr = GameEngineVertexShaderManager::GetInst().Load(FileName + "_VS", ShaderFile.GetFullPath(), FileName + "_VS");
+	//	}
+	//
+	//	if (std::string::npos != AllCode.find(FileName + "_PS"))
+	//	{
+	//		GameEnginePixelShader* Ptr = GameEnginePixelShaderManager::GetInst().Load(FileName + "_PS", ShaderFile.GetFullPath(), FileName + "_PS");
+	//	}
+	//}
 
 
 	// GameEngineVertexShaderManager::GetInst().Load(Dir.PathToPlusFileName("Color.fx"), "Color_VS");
