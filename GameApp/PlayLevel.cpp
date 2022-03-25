eef1a3a4f7dd0d566f566addb0383ea54385d395
Won Park <nerichagi21@gmail.com>
2022-03-25 17:40:23 +0900

    플레이레벨 마우스 나타내기

--- GameApp/PlayLevel.cpp
+++ GameApp/PlayLevel.cpp
@@ -4,6 +4,7 @@
 #include <GameEngine/CameraComponent.h>
 #include <GameEngine/GameEngineTransform.h>
 #include <GameEngine/CameraActor.h>
+#include "GameMouse.h"
 
 #include "GameController.h"
 #include "UIController.h"
@@ -21,6 +22,12 @@ void PlayLevel::LevelStart()
 {
 	GetMainCamera()->SetProjectionMode(ProjectionMode::Orthographic);
 	GetMainCamera()->GetTransform()->SetLocalPosition(float4(0.0f, 0.0f, -100.0f));
+
+	{
+		// 타이틀씬의 마우스 액터입니다.
+		GameMouse* mouseActor = CreateActor<GameMouse>();
+		mouseActor->GetUIRenderer()->SetRenderGroup(static_cast<int>(RenderOrder::UI0));
+	}
 
 	{
 		GameController* gameController = CreateActor<GameController>();
